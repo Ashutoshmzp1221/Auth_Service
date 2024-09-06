8c7351123eb3ce9db7abd4e3bb540060f10c1158
Ashutosh Dwivedi <ashutoshmzp1221@gmail.com>
2024-09-06 18:43:50 +0530

    Added token generation logic through sign in

--- src/controllers/user-controllers.js
+++ src/controllers/user-controllers.js
@@ -8,7 +8,27 @@ const create = async (req, res) => {
         });
         return res.status(201).json({
             success: true,
-            message: 'Successfully created anew user',
+            message: 'Successfully created a new user',
+            data: response,
+            err: {}
+        })
+    } catch (error) {
+        console.log(error);
+        return res.status(500).json({
+            message: 'Something went wrong',
+            data: {},
+            success: false,
+            err: error
+        });
+    }
+}
+
+const signIn = async (req, res) => {
+    try {
+        const response = await userService.signIn(req.body.email, req.body.password);
+        return res.status(200).json({
+            success: true,
+            message: 'Successfully signed in',
             data: response,
             err: {}
         })
@@ -24,5 +44,6 @@ const create = async (req, res) => {
 }
 
 module.exports = {
-    create
+    create,
+    signIn
 }
